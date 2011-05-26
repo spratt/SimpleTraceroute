b9836b8dca6d1c84afc728d43ee1d479cabef67a
Simon Pratt <spratt@scs.carleton.ca>
2011-05-26 10:18:18 -0400

    Added a LICENSE file, wrote a proper README and added lots of comments

--- tr.sh
+++ tr.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+################################################################################
+# Simple Traceroute
+#
+# For project information, see file: README
+# For license information, see file: LICENSE
+################################################################################
 for ((i=1; i <= $2; i++)); do
+    # Not sure, but I guess if ping doesn't go through
+    # grep will ignore any error lines
     response=`ping -n -c 1 -t $i $1 | grep From --`
+    
+    # A successful response looks like:
+    #  From a.b.c.d icmp_seq=1 Time to live exceeded
+    
+    # Strip the "From "
     response=${response:5}
+    # Strip the " icmp_seq=1 Time to live exceeded"
     response=${response%% icmp_seq=1 Time to live exceeded}
+    # print the hop number and address
     echo $i: $response
 done
