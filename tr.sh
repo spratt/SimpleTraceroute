399c6e913f4027765d9110b6c90ab089a7bf56a5
Simon Pratt <spratt@scs.carleton.ca>
2011-05-27 14:33:01 -0400

    Now waiting a maximum of 2 seconds for each ping to return

--- tr.sh
+++ tr.sh
@@ -8,7 +8,13 @@
 for ((i=1; i <= $2; i++)); do
     # Not sure, but I guess if ping doesn't go through
     # grep will ignore any error lines
-    response=`ping -n -c 1 -t $i $1 | grep From --`
+    response=`ping -n -w 2 -c 1 -t $i $1 | grep From --`
+    # arguments:
+    #   -n:    numerical output only (don't resolve ips)
+    #   -w 2:  wait a maximum of 2 seconds for the ping to return
+    #   -c 1:  send one packet
+    #   -t $i: TTL=$i
+    #   $1:    some host to ping
     
     # A successful response looks like:
     #  From a.b.c.d icmp_seq=1 Time to live exceeded
